d9966fa80232b33e70512a9b653ef935fa66efc3
山本 博之 <bigupsanche@gmail.com>
2018-10-18 13:13:36 +0900

    icon編集

--- _material/gulpfile.js
+++ _material/gulpfile.js
@@ -1,23 +1,19 @@
 
 // gulpプラグインの読み込み
 var gulp = require('gulp');
-var rename = require("gulp-rename");
 var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', function () {
   // ★ style.scssファイルを監視
   gulp.watch('scss/**/*.scss', function () {
     // style.scssの更新があった場合の処理
-  
     // style.scssファイルを取得
     gulp.src('scss/basic-style.scss')
       // Sassのコンパイルを実行
       .pipe(sass({
         outputStyle: 'expanded'
       })
-      // Sassのコンパイルエラーを表示
-      // (これがないと自動的に止まってしまう)
+      // Sassのコンパイルエラーを表示 (これがないと自動的に止まってしまう)
       .on('error', sass.logError))
       // cssフォルダー以下に保存
       .pipe(gulp.dest('../assets/basic-assets'));
