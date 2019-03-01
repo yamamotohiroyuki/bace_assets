15ed0b00b51c4229e242e4bc6568c1a89f4ebb32
山本 博之 <bigupsanche@gmail.com>
2019-03-01 12:04:10 +0900

    bug fix

--- _material/gulpfile.js
+++ _material/gulpfile.js
@@ -30,8 +30,8 @@ var bundle_array = [
   submodule_path+'slick/slick/slick.js',
   submodule_path+'slicknav/dist/jquery.slicknav.js',
   submodule_path+'sticky-kit/dist/sticky-kit.js',
-  'javascript/original/currentsetting.js',
-  'javascript/original/acs.js',
+  'javascript/vendor/original/currentsetting.js',
+  'javascript/vendor/original/acs.js',
   'javascript/basic-bundle.js'
 ];
 var adjust_array = [
